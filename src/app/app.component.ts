932f963f7f35cf7fe8269838bf553b1c9d1a205d
agershman <aarakokra@gmail.com>
2023-10-06 17:51:21 +0300

    Предупреждает об отсутствии куки и/или JavaScript. Также установлены свои заголовок и значок.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Urbrato-Todo-Frontend';
+export class AppComponent implements OnInit {
+  cookieEnabled: boolean = false;
+
+  ngOnInit(): void {
+    this.cookieEnabled = navigator.cookieEnabled;
+
+    // на случай, если navigator не поддерживается
+    if (!this.cookieEnabled) {
+      document.cookie = 'test_cookie';
+      this.cookieEnabled = document.cookie.indexOf('test_cookie') !== -1;
+    }
+  }
 }
