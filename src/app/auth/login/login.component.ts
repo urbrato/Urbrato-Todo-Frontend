0a58274eac0e2a0f8e28137d55c50425bc3e7393
agershman <aarakokra@gmail.com>
2023-10-31 01:14:07 +0300

    Скелет формы входа

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  form: FormGroup;
+
+  get usernameField(): AbstractControl {
+    return this.form!.get('username')!;
+  }
+
+  get passwordField(): AbstractControl {
+    return this.form!.get('password')!;
+  }
+
+  submitForm() {
+    alert(this.form + '|' + this.usernameField.value + '|' + this.passwordField.value);
+  }
+
+  ngOnInit(): void {
+  }
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
 }
