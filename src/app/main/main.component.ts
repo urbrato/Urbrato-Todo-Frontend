39888b41db3623dd97782fd0530566c18c00d691
agershman <agershman@corp.local>
2024-01-24 11:52:53 +0300

    Работа с боковой панелью доведена до ума

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,21 +1,31 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {User} from "../entities/user";
 import {AuthService} from "../service/auth.service";
 import {ProfileService} from "../service/profile.service";
 import {Router} from "@angular/router";
+import {DeviceDetectorService} from "ngx-device-detector";
+import {MatDrawerMode} from "@angular/material/sidenav";
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
   currentUser: User | null = null;
+
+  isMobile: boolean = false;
+  isTablet: boolean = false;
+  isDesktop: boolean = false;
+
+  catsOpened: boolean = false;
+  catsMode: MatDrawerMode = "side";
 
   constructor(
     private srvAuth: AuthService,
     private srvProfile: ProfileService,
-    private router: Router) {
+    private router: Router,
+    private deviceDetector: DeviceDetectorService) {
 
     srvAuth.currentUser.subscribe(user => {
       this.currentUser = user;
@@ -32,6 +42,27 @@ export class MainComponent {
           this.router.navigate(['']).then(_ => {});
         }
       });
+    }
+  }
+
+  ngOnInit(): void {
+    this.detectDevice();
+    this.initCatsDrawer();
+  }
+
+  detectDevice() {
+    this.isMobile = this.deviceDetector.isMobile();
+    this.isTablet = this.deviceDetector.isTablet();
+    this.isDesktop = this.deviceDetector.isDesktop();
+  }
+
+  initCatsDrawer() {
+    if (this.isMobile) {
+      this.catsOpened = false;
+      this.catsMode = "over";
+    } else {
+      this.catsOpened = true;
+      this.catsMode = "push";
     }
   }
 }
