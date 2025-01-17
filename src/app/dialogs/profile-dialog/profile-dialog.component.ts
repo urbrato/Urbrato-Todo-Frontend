1ddf4845434e6fcdf5273d13bbc3cc3a7c5a668a
Гершман Антон Лейбович <allegecityrat@yandex.ru>
2025-01-17 03:14:47 +0300

    Окошко личного кабинета смакетировано

--- src/app/dialogs/profile-dialog/profile-dialog.component.ts
+++ src/app/dialogs/profile-dialog/profile-dialog.component.ts
@@ -3,18 +3,20 @@ import {MatDialogActions, MatDialogContent, MatDialogTitle} from "@angular/mater
 import {TranslateModule} from "@ngx-translate/core";
 import {MatButton} from "@angular/material/button";
 import {MatSlideToggle} from "@angular/material/slide-toggle";
+import {MatFormField} from "@angular/material/form-field";
 
 @Component({
   selector: 'app-profile-dialog',
   standalone: true,
-  imports: [
-    MatDialogContent,
-    MatDialogActions,
-    MatDialogTitle,
-    TranslateModule,
-    MatButton,
-    MatSlideToggle
-  ],
+    imports: [
+        MatDialogContent,
+        MatDialogActions,
+        MatDialogTitle,
+        TranslateModule,
+        MatButton,
+        MatSlideToggle,
+        MatFormField
+    ],
   templateUrl: './profile-dialog.component.html',
   styleUrl: './profile-dialog.component.css'
 })
